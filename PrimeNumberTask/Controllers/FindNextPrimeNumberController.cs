8ea4c2f197e6c63a632fa78bb05e92f5001a7da4
Banana1 <nqmammail@shop.com>
2020-09-21 14:59:29 +0300

    change the http status code to 500 when an unknown exception is thrown

--- PrimeNumberTask/Controllers/FindNextPrimeNumberController.cs
+++ PrimeNumberTask/Controllers/FindNextPrimeNumberController.cs
@@ -6,7 +6,6 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using PrimeNumberTask.Controllers.Constants;
 using PrimeNumberTask.Controllers.Models;
 using PrimeNumberTask.Lib;
@@ -18,35 +17,32 @@ namespace PrimeNumberTask.Controllers
     [ProducesResponseType(typeof(OperationResult), 200)]
     [ProducesResponseType(typeof(ValidationProblemDetails), 400)]
     [ProducesResponseType(typeof(ProblemDetails), 500)]
-    public class IsPrimeNumberController : ControllerBase
+    public class FindNextPrimeNumberController : ControllerBase
     {
-
         [HttpGet("{number}")]
-        public ActionResult<OperationResult> CheckIfNumberIsPrime([Required] decimal number)
+        public ActionResult<OperationResult> FindNextPrimeNumber([Required] decimal number)
         {
             if (number > 2147483647)
                 return ValidationProblem(
                     new ValidationProblemDetails(
-                        new Dictionary<string, string[]> { 
-                            { 
-                                "number", new string[] { "Only numbers between 2 and 2,147,483,647 are accepted." } 
-                            } 
+                        new Dictionary<string, string[]> {
+                            {
+                                "number", new string[] { "Only numbers between 2 and 2,147,483,647 are accepted." }
+                            }
                         }
                     )
                 );
 
             try
-            {                
-                bool isPrime = PrimeNumberHelper.IsPrimeNumber((int)number);
+            {
+                int nextPrime = PrimeNumberHelper.FindNextPrimeNumberBiggerOrEqual((int)number);
 
                 return Ok(new OperationResult(
-                    OperationNames.IsPrimeNumber,
+                    OperationNames.FindNextPrimeNumberBiggerOrEqual,
                     new Dictionary<string, object>() { { "number", number } },
-                    isPrime ? 
-                    $"The number {number} is a prime." :
-                    $"The number {number} is a composite number."));
+                    $"{nextPrime} is the next prime number."));
             }
-            catch(ArgumentOutOfRangeException argumentEx)
+            catch (ArgumentOutOfRangeException argumentEx)
             {
                 return ValidationProblem(
                     new ValidationProblemDetails(
@@ -58,7 +54,7 @@ namespace PrimeNumberTask.Controllers
                     )
                 );
             }
-            catch(Exception)
+            catch (Exception)
             {
                 return StatusCode((int)HttpStatusCode.InternalServerError, new ProblemDetails()
                 {
@@ -66,7 +62,7 @@ namespace PrimeNumberTask.Controllers
                     Detail = ResponseErrors.UnknownError,
                     Title = "unknownError"
                 });
-            }            
+            }
         }
     }
 }
